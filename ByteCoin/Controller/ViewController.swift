cf8873ef9e0943226a972a38e073192815b2098d
djmads08 <47112428+djmads08@users.noreply.github.com>
2021-09-07 08:13:02 +0100

    Added API key for CoinApi.io
    Adds support for API key at CoinApi.io, now requires parsing.

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -21,18 +21,23 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return 1
     }
     
+    // Initialise the coinManager to access methods and variables.
     let coinManager = CoinManager()
     
+    // Define the number of rows in the pickerView based on the count of the currencyArray constant in coinManager.
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        
+         
         return coinManager.currencyArray.count
     }
     
+    // Define the title of the row, taking in a row number and returning a String.
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
         return coinManager.currencyArray[row]
     }
     
+    // Defining the selected Currency of the app based on the pickerView chosen. This output is sent to the coinManager's getCoinPrice
+    // method to ask the API for the current price in selectedCurrency.
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let selectedCurrency = coinManager.currencyArray[row]
         coinManager.getCoinPrice(for: selectedCurrency)
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,12 +11,49 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "05908BDB-30DA-47EF-B9FC-FB09B121F9BD"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
+        let urlPart1 = "/\(currency)"
+        let urlPart2 = "?apikey=\(apiKey)"
+        let urlString = baseURL + urlPart1 + urlPart2
+        performRequest(urlString: urlString)
+        print(urlString)
+        return
+    }
+    
+    func performRequest(urlString: String) {
+        // 1. Create a URL
         
+        if let url = URL(string:urlString) {
+            
+            // 2. Create a URL Session
+            let session = URLSession (configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+    
+    func didUpdateCoinPrice() -> Bool {
+        return true
     }
     
 }
